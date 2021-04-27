58db76ae87eaedb696bdb6ef8b216f226b366046
Emmanuel <emmanuelamon92@gmail.com>
2021-04-27 18:56:19 -0400

    commit tree a1e74c6bba4952f2c3f97eb8c5aa45e1179dbb0c

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = parseInt(Math.random() + 1)
+
+let num3 = 4
+let num4 = 16
+
+let mod = num3 % num4
+
+let maxNum = 20
+let max = Math.max(maxNum)
